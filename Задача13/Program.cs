7d92995a18937427aadf05658599f959fabad040
abotnari80 <abotnari80@gmail.com>
2022-03-06 13:37:17 +0300

    Задача 13

---
+++ Задача13/Program.cs
@@ -0,0 +1,22 @@
+﻿int thirdNumber(int x) {
+
+    if (x > 99) {
+
+        while (x > 999) {
+            x = x / 10;
+        }
+
+        int answer = x % 10;
+        return(answer);
+    }
+
+    else {
+        return(10);
+    }
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int ans = thirdNumber(number);
+if (ans == 10) Console.WriteLine("Третьей цифры нет");
+else Console.WriteLine(ans);
