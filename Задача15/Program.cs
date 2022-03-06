418710b3b28c95b06d369f219f85d86e26d77643
abotnari80 <abotnari80@gmail.com>
2022-03-06 13:39:38 +0300

    Задача 15

---
+++ Задача15/Program.cs
@@ -0,0 +1,24 @@
+﻿string weekends(int day)
+{
+    string weekend = "да";
+    string workday = "нет";
+    string error = "такого дня недели нет";
+
+    if (day > 0 && day < 8)
+    {
+        if (day == 6 || day ==7)
+            return weekend;
+        else
+            return workday;
+    }
+    else
+    {
+        return error;
+    }
+
+}
+
+Console.WriteLine("Введите номер дня недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+string result = weekends(dayNumber);
+Console.WriteLine(result);
