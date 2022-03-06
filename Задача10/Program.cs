c7d2fe996b03f6042a1760aa049077c3d7f19f54
abotnari80 <abotnari80@gmail.com>
2022-03-06 13:33:34 +0300

    Задача 10

---
+++ Задача10/Program.cs
@@ -0,0 +1,12 @@
+﻿int middleNumber(int num)
+{
+    num = num / 10;
+    int answer = num % 10;
+    return answer;
+}
+
+Console.WriteLine("Введите 3-х значное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = middleNumber(number);
+
+Console.WriteLine(result);
